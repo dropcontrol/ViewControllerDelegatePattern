72476fcde2ca7b9a90108452a93f2033455c439f
hiroshi yamato <yamato@gmail.com>
2020-11-05 11:10:27 +0900

    Initial Commit

---
+++ ViewControllerDelegatePattern/ViewControllerDelegatePatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerDelegatePatternApp.swift
+//  ViewControllerDelegatePattern
+//
+//  Created by hiroshi yamato on 2020/11/05.
+//
+
+import SwiftUI
+
+@main
+struct ViewControllerDelegatePatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
